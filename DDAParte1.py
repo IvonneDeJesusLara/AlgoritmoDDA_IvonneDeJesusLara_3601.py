b7c89127840a0e970f56c4665df309eada83a572
IvonneDeJesusLara <61301331+IvonneDeJesusLara@users.noreply.github.com>
2021-05-20 18:25:26 -0500

    Add files via upload

---
+++ DDAParte1.py
@@ -0,0 +1,12 @@
+from DDAParte2 import Graficar
+def main():
+    x1= int(input('INGRESA VALOR PARA X1:\n'))
+    y1= int(input('INGRESA VALOR PARA X2:\n'))
+    x2= int(input('INGRESA VALOR PARA Y1:\n'))
+    y2= int(input('INGRESA VALOR PARA Y2:\n'))
+
+    Graficar(x1, y1, x2, y2)
+
+if __name__== '__main__':
+    main()
+
